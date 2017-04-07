277ea7682f8cf9e7997ea2683169ec2d04a3238c
Daniel Douglas Machado <danielprogramic@gmail.com>
2017-04-07 17:38:29 -0300

    insert router

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
-
 var app = express();
-
 var port = 5000;
-
+var bookRouter = express.Router();
 //inserindo os diretórios estáticos.
 // o app use nos permite configurar algum middleware
 app.use(express.static('public'));
@@ -16,9 +14,67 @@ var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 */
+var books = [{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevick Tolstoy',
+    read: false
+},{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevick Tolstoy',
+    read: false
+},{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevick Tolstoy',
+    read: false
+},{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevick Tolstoy',
+    read: false
+}
+];
+
+bookRouter.route('/')
+    .get(function(req, res) {
+        //res.send('Hello Books');
+        res.render('books', {
+        title: 'Hello from render',
+        nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            },
+            {
+                Link: '/Authors',
+                Text: 'Authors'
+            }
+        ],
+        book: books
+    });
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res) {
+        res.send('Hello Single Books');
+    });
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res) {
-    res.render('index', { title: 'Hello from render', list: ['a', 'b', 'c'] });
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+                Link: '/Books',
+                Text: 'Books'
+            },
+            {
+                Link: '/Authors',
+                Text: 'Authors'
+            }
+        ]
+    });
 });
 
 app.get('/books', function(req, res) {
